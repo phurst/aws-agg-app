44858a4a921d346a48c4178a85f16e64c6eac2d1
phurst <5099571+phurst@users.noreply.github.com>
2025-07-17 15:04:50 -0400

    Update Schema to add the AggReading external table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,6 +9,15 @@ const schema = a.schema({
       value: a.float(),
     })
     .authorization((allow) => [allow.owner()]),
+
+  AggReading: a.customType({
+    id: a.id().required(),
+    sensorGroup: a.integer(),
+    sensorNumber: a.integer(),
+    time: a.datetime(),
+    valueType: a.integer(),
+    value: a.float(),
+  }),
 });
 
 export type Schema = ClientSchema<typeof schema>;
